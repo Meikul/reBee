c1422e86d5554375ee8de1506ea4f7d213d7016f
Michael Harris <meikul.harris@gmail.com>
2022-02-10 19:22:22 -0700

    testing today game

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ const state = {
 }
 
 async function windowLoaded() {
-  updateInputBox()
   const gameData = await getGame()
   console.log(gameData);
   state.gameData = gameData
@@ -18,127 +17,130 @@ async function windowLoaded() {
 
   const hexes = document.querySelectorAll('.hex')
   hexes.forEach(hex => {
-    hex.addEventListener('touchstart', hexClick)
+    hex.addEventListener('touchstart', hexPress)
     hex.addEventListener('touchend', hexRelease)
-    hex.addEventListener('mousedown', hexClick)
+    hex.addEventListener('mousedown', hexPress)
     hex.addEventListener('mouseup', hexRelease)
     hex.addEventListener('mouseleave', hexRelease)
   })
   document.addEventListener('keydown', keyPress)
+  updateInputBox()
+  updateScore()
 }
 
 async function getGame(type = 'random') {
   if(type !== 'today' && type !== 'random') return
   // const response = fetch(`https://freebee.fun/cgi-bin/random`)
-  // return (await response).json()
-  const response = {
-    center: "h",
-    letters: "ecrdoi",
-    total: 530,
-    wordlist: [
-    "cheder",
-    "cheer",
-    "cheered",
-    "cheerer",
-    "cheerier",
-    "cheerio",
-    "cheero",
-    "chic",
-    "chicer",
-    "chichi",
-    "chico",
-    "chid",
-    "chide",
-    "chided",
-    "chider",
-    "chiro",
-    "chirr",
-    "chirre",
-    "chirred",
-    "choice",
-    "choicer",
-    "choir",
-    "choired",
-    "chord",
-    "chorded",
-    "chore",
-    "chored",
-    "choreic",
-    "choreoid",
-    "choric",
-    "chorioid",
-    "choroid",
-    "coheir",
-    "cohere",
-    "cohered",
-    "coherer",
-    "coho",
-    "cooch",
-    "creche",
-    "dichroic",
-    "dreich",
-    "eche",
-    "eched",
-    "echo",
-    "echoed",
-    "echoer",
-    "echoic",
-    "heder",
-    "heed",
-    "heeded",
-    "heeder",
-    "heir",
-    "heired",
-    "herd",
-    "herded",
-    "herder",
-    "herdic",
-    "here",
-    "hero",
-    "heroic",
-    "herried",
-    "hide",
-    "hided",
-    "hider",
-    "hied",
-    "hire",
-    "hired",
-    "hirer",
-    "hoed",
-    "hoer",
-    "hooch",
-    "hood",
-    "hooded",
-    "hoodie",
-    "hoodier",
-    "hoodoo",
-    "hoodooed",
-    "horde",
-    "horded",
-    "horrid",
-    "horror",
-    "ichor",
-    "ocher",
-    "ochered",
-    "ochre",
-    "ochred",
-    "ochroid",
-    "ohed",
-    "oohed",
-    "orchid",
-    "recherche",
-    "reechier",
-    "reecho",
-    "reechoed",
-    "rehire",
-    "rehired",
-    "rhodic",
-    "rich",
-    "richer"
-    ],
-    words: 99,
-  }
-  return response
+  const response = fetch(`https://freebee.fun/play/today`)
+  return (await response).json()
+  // const response = {
+  //   center: "h",
+  //   letters: "ecrdoi",
+  //   total: 530,
+  //   wordlist: [
+  //   "cheder",
+  //   "cheer",
+  //   "cheered",
+  //   "cheerer",
+  //   "cheerier",
+  //   "cheerio",
+  //   "cheero",
+  //   "chic",
+  //   "chicer",
+  //   "chichi",
+  //   "chico",
+  //   "chid",
+  //   "chide",
+  //   "chided",
+  //   "chider",
+  //   "chiro",
+  //   "chirr",
+  //   "chirre",
+  //   "chirred",
+  //   "choice",
+  //   "choicer",
+  //   "choir",
+  //   "choired",
+  //   "chord",
+  //   "chorded",
+  //   "chore",
+  //   "chored",
+  //   "choreic",
+  //   "choreoid",
+  //   "choric",
+  //   "chorioid",
+  //   "choroid",
+  //   "coheir",
+  //   "cohere",
+  //   "cohered",
+  //   "coherer",
+  //   "coho",
+  //   "cooch",
+  //   "creche",
+  //   "dichroic",
+  //   "dreich",
+  //   "eche",
+  //   "eched",
+  //   "echo",
+  //   "echoed",
+  //   "echoer",
+  //   "echoic",
+  //   "heder",
+  //   "heed",
+  //   "heeded",
+  //   "heeder",
+  //   "heir",
+  //   "heired",
+  //   "herd",
+  //   "herded",
+  //   "herder",
+  //   "herdic",
+  //   "here",
+  //   "hero",
+  //   "heroic",
+  //   "herried",
+  //   "hide",
+  //   "hided",
+  //   "hider",
+  //   "hied",
+  //   "hire",
+  //   "hired",
+  //   "hirer",
+  //   "hoed",
+  //   "hoer",
+  //   "hooch",
+  //   "hood",
+  //   "hooded",
+  //   "hoodie",
+  //   "hoodier",
+  //   "hoodoo",
+  //   "hoodooed",
+  //   "horde",
+  //   "horded",
+  //   "horrid",
+  //   "horror",
+  //   "ichor",
+  //   "ocher",
+  //   "ochered",
+  //   "ochre",
+  //   "ochred",
+  //   "ochroid",
+  //   "ohed",
+  //   "oohed",
+  //   "orchid",
+  //   "recherche",
+  //   "reechier",
+  //   "reecho",
+  //   "reechoed",
+  //   "rehire",
+  //   "rehired",
+  //   "rhodic",
+  //   "rich",
+  //   "richer"
+  //   ],
+  //   words: 99,
+  // }
+  // return response
 }
 
 function fillHexes(letters, center) {
@@ -148,14 +150,17 @@ function fillHexes(letters, center) {
   }
 }
 
-function hexClick(){
+function hexPress(e){
+  e.preventDefault()
   const letter = this.querySelector('.hex-letter')
   typeLetter(letter.innerHTML)
   this.classList.add('activated')
 }
 
 function hexRelease() {
-   this.classList.remove('activated')
+  if(this.classList.contains('activated')){
+    this.classList.remove('activated')
+  }
 }
 
 function keyPress(e) {
@@ -169,9 +174,33 @@ function updateInputBox() {
   document.getElementById('text-input').innerHTML = state.currentInput.toUpperCase()
 }
 
-function updateScoreBox() {
-  console.log(state.score);
-  document.getElementById('score-display').innerHTML = state.score
+function appendWordList(word) {
+  document.getElementById('word-count-number').innerHTML = state.foundWords.length
+
+  const capitalizedWord = word[0].toUpperCase() + word.slice(1)
+
+  const list = document.getElementById('found-list')
+  const entry = document.createElement('li')
+  entry.appendChild(document.createTextNode(capitalizedWord))
+  list.appendChild(entry)
+}
+
+function updateScore() {
+  document.getElementById('score-number').innerHTML = state.score
+
+
+  const scoreRatio = state.score / state.gameData.total
+  let rank = ''
+  if(scoreRatio > 0.7) rank = 'Queen Bee'
+  else if(scoreRatio > 0.50) rank = 'Outstanding'
+  else if(scoreRatio > 0.40) rank = 'Marvellous'
+  else if(scoreRatio > 0.25) rank = 'Superb'
+  else if(scoreRatio > 0.15) rank = 'Excellent'
+  else if(scoreRatio > 0.08) rank = 'Skilled'
+  else if(scoreRatio > 0.05) rank = 'Fine'
+  else if(scoreRatio > 0.02) rank = 'Novice'
+  else rank = 'Newbie'
+  document.getElementById('rank').innerHTML = rank
 }
 
 function typeLetter(letter) {
@@ -181,28 +210,41 @@ function typeLetter(letter) {
 }
 
 function showError(message) {
-  const errBox = document.getElementById('error-message')
-  errBox.innerHTML = message
-  errBox.classList.add('active')
+  const msgBox = document.getElementById('message-box')
+  msgBox.innerHTML = message
+  msgBox.classList.add('error')
   setTimeout(() => {
-    const errBox = document.getElementById('error-message')
-    errBox.classList.remove('active')
-  }, 700);
+    const msgBox = document.getElementById('message-box')
+    msgBox.classList.remove('error')
+  }, 1000);
+}
+
+function showMessage(message) {
+  const msgBox = document.getElementById('message-box')
+  msgBox.innerHTML = message
+  msgBox.classList.add('message')
+  setTimeout(() => {
+    const errBox = document.getElementById('message-box')
+    errBox.classList.remove('message')
+  }, 1000);
 }
 
 function checkWord() {
-  console.log(state.currentInput);
   if(state.currentInput.length < 4){
     console.log('too short');
     showError('Word too short')
+  }
+  else if(state.foundWords.includes(state.currentInput)){
+    console.log('already found');
+    showError('Word already found')
   }
   else if(hasInvalidLetter(state.currentInput)){
     console.log('invalid letter');
     showError('Invalid letter')
   }
-  else if(state.foundWords.includes(state.currentInput)){
-    console.log('already found');
-    showError('Word already found')
+  else if(!state.currentInput.includes(state.gameData.center)){
+    console.log('no center');
+    showError('Missing center letter')
   }
   else if(!state.gameData.wordlist.includes(state.currentInput)){
     console.log('not on list');
@@ -211,10 +253,11 @@ function checkWord() {
   else{
     state.score += scoreWord(state.currentInput)
     state.foundWords.push(state.currentInput)
+    appendWordList(state.currentInput)
   }
   state.currentInput = ''
+  updateScore()
   updateInputBox()
-  updateScoreBox()
 }
 
 function hasInvalidLetter(word) {
@@ -246,6 +289,7 @@ function scoreWord(word) {
   }
   if(isPanagram){
     console.log('Panagram');
+    showMessage('Panagram!')
     score += 7
   }
 
